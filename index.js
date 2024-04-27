a952c565f7b7a6025f53facdfe5d9d2f79b4ba50
sergiorsj11 <sergiorsj11@gmail.com>
2024-04-27 18:56:40 -0300

    auth and passport js files
    exer 2.9

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ const uuid = require('uuid');
 
 app.use(bodyParser.json());
 
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./passport');
+
 let users = [
     {
       id:'1',
