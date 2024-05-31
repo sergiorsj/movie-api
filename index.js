f90179ddade0a92bbcd83525206883934c049470
sergiorsj11 <sergiorsj11@gmail.com>
2024-05-30 21:43:56 -0300

    removed cors
    working w/ John

--- index.js
+++ index.js
@@ -34,8 +34,6 @@ const uuid = require("uuid");
 app.use(bodyParser.json());
 
 let auth = require("./auth")(app);
-const cors = require("cors");
-app.use(cors());
 const passport = require("passport");
 require("./passport");
 
